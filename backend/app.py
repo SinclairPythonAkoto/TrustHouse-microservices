b85dca147bfde7b4768b9a6aa454a1ed6e73f2cb
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-01-17 02:38:16 +0000

    backend added to find all locations and find reviews by rating; full logic not completed

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ HOST = os.environ['HOST']
 PORT = os.environ['BACKEND_PORT']
 
 
-# define all routes
+# backend for all routes
 
 
 @app.route('/')
@@ -21,9 +21,34 @@ def home():
     return "hello world"
 
 
+# create a new review
 @app.route('/new-review', methods=['POST'])
 def new_review():
     return 'Create a new review'
+
+
+# display all reviews
+@app.route('/review/all', methods=['POST'])
+def all_reviews():
+    return 'Find all reviews'
+
+
+# display all review locations
+@app.route('/review/locations', methods=['POST'])
+def find_locations():
+    return 'Find all locations'
+
+
+# display all reviews by rating 
+@app.route('/review/rating', methods=['POST'])
+def find_by_rating():
+    rating = request.form['searchRating']
+    return f'Find all reviews with a rating of {rating}'
+
+
+# 
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host=HOST, port=PORT)
